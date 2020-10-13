7d6ba275dad66a64d76873c65d831c124793f98a
gregoritoo <gregory.scafarto@gmail.com>
2020-10-13 16:08:46 +0100

    stacked barchart

--- index.js
+++ index.js
@@ -1,53 +1,76 @@
+var margin = {top: 60, right: 40, bottom: 0, left: 50},
+    width = 1000,
+    height = 600 ;
+    keys = ["lum1", "lum2", "lum3", "lum4", "lum5"],
+    colors = ["#f7fcf0", "#e0f3db", "#ccebc5", "#a8ddb5", "#7bccc4"],
+    legendCellSize = 20,
+    tooltipWidth = 210;
+
+
+
 var svg = d3.select("#chart-bar").append("svg")
-				.attr("height",800)
-				.attr("width",800) ;
-var width = 1200;
-var height = 300;
+    .attr("id", "svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv('mois_lum.csv').then(data => {
 
 
-var margin = {top:20,bottom:20,left:600,right:20} ;
+        var stack = d3.stack()
+        .keys(keys)
+        .order(d3.stackOrderNone)
+        .offset(d3.stackOffsetNone);
 
-var innerWidth = width - margin.left - margin.right ;
+    var series = stack(data);
 
-var innerHeight = height - margin.top - margin.bottom ;
+    console.log(series)
 
-var xValue = data => data.count;
-var yValue = data => data.atm;
-var render_barchart = data => {
-	var xscale = d3.scaleLinear()
-		.domain([0,d3.max(data,xValue)])
-		.range([0,innerWidth]);
+    const x = d3.scaleBand()
+        .domain(data.map(d => d.mois))
+        .range([0, width/1.5])
+        .padding(0.1);
 
-	var yscale = d3.scaleBand()
-		.domain(data.map(yValue))
-		.range([0,innerHeight])
-		.padding(0.4);
+    const y = d3.scaleLinear()
+        .domain([0, 6000])
+        .range([height, 0]);
+        
+    // Sur l'axe horizontal, on filtre les dates afficher
+    const xAxis = d3.axisBottom(x)
+        .tickValues(x.domain());
 
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .selectAll("text")  
+        .style("text-anchor", "middle");
 
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .style("text-anchor", "end")
+         .text("Nombre d'accidents")
+         .attr("transform","translate(50,-20)");
 
-	var g2 =svg.append("g")
-		.attr('transform','translate('+110+','+margin.top+')');
-		
-	
-	g2.selectAll('rect').data(data)
-		.enter().append('rect')
-		.attr('y',data=>yscale(yValue(data)))
-		.attr('width',data=>xscale(xValue(data)))
-		.attr('height',yscale.bandwidth())
-		.attr('fill',"#6C3483");
+    let groups = svg.selectAll("g.lum")
+        .data(series)
+        .enter().append("g")
+        .style("fill", (d, i) => colors[i]);
 
-	var yaxis = d3.axisLeft().scale(yscale) ;
-	var xaxis = d3.axisBottom().scale(xscale) ;
+    let rect = groups.selectAll("rect")
+        .data(d => d)
+        .enter()
+            .append("rect")
+            .attr("x", d => x(d.data.mois))
+            .attr("width", x.bandwidth())
+            .attr("y", d => y(d[1]))
+            .attr("height", d => height - y(d.data.lum1- d.data.mois));
 
-	g2.append("g").call(xaxis).attr('transform','translate('+0+','+260+')');
-	g2.append("g").call(yaxis).attr('transform','translate('+0+','+0+')');
-};
-
-d3.csv('data_atm.csv').then(data => 
-	{
-	data.forEach( data => {
-		data.count=+data.count;
-	});
-
-	render_barchart(data);
 });
